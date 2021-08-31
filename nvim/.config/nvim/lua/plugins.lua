d5c46d7f6de9ae2b0a21bf287fcb387a4d005df6
Robin Van den Broeck <robin.broeck@gmail.com>
2021-08-31 13:34:03 +0200

    Add keybindings for lsp actions

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,16 +7,54 @@ if fn.empty(fn.glob(install_path)) > 0 then
     vim.cmd "packadd packer.nvim"
 end
 
--- Recompile packer whenever plugins.lua changes
-vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
+
+lsp_setup_buffer_keymap = function(wk)
+    return function(_client, bufnr)
+        print("setting up mapping")
+        local buf = vim.lsp.buf
+
+        wk.register(
+            {
+                g = {
+                    d = {buf.definition, "definition"},
+                    D = {buf.declaration, "declaration"},
+                    i = {buf.implementation, "implementation"},
+                    r = {buf.references, "references"}
+                },
+                K = {buf.hover, "Hover"},
+                ["<leader>"] = {
+                    c = {
+                        name = "code",
+                        a = {buf.code_action, "action"}
+                    },
+                    rn = {buf.rename, "rename"},
+                    rf = {buf.formatting, "reformat"},
+                    w = {
+                        name = "workspace",
+                        s = {":Telescope lsp_workspace_symbols<CR>", "symbols"}
+                    }
+                }
+            },
+            {
+                mode = "n",
+                buffer = bufnr
+            }
+        )
+    end
+end
+
+lsp_get_capabilities = function(cmp_nvim_lsp)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    return cmp_nvim_lsp.update_capabilities(capabilities)
+end
 
 return require("packer").startup(
     function()
         -- Let packer manage itself, so it can update.
         use "wbthomason/packer.nvim"
 
-        -- General
-        use "editorconfig/editorconfig-vim"
+        -- General use "editorconfig/editorconfig-vim"
 
         -- UI
         use {"dracula/vim", as = "dracula"}
@@ -74,20 +112,28 @@ return require("packer").startup(
         use {
             "neovim/nvim-lspconfig",
             config = function()
-                -- TODO: add languages
-                -- Rust get's handled by rust tools
+                -- TODO: add more servers
+                -- rust is handled in rust-tools
             end
         }
 
         -- Autocompletion
         use {
+            "onsails/lspkind-nvim",
+            config = function()
+                require("lspkind").init {}
+            end
+        }
+
+        use {
             "hrsh7th/nvim-cmp",
             requires = {
                 "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-nvim-lsp"
+                "hrsh7th/cmp-nvim-lsp",
+                "onsails/lspkind-nvim"
             },
             config = function()
-                vim.o.completeopt = "menuone,noselect"
+                local lspkind = require("lspkind")
 
                 local fn = vim.fn
                 local replace_termcodes = vim.api.nvim_replace_termcodes
@@ -120,6 +166,15 @@ return require("packer").startup(
                             end
                         end
                     },
+                    format = function(entry, vim_item)
+                        vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+                        vim_item.menu =
+                            ({
+                            nvim_lsp = "[LSP]",
+                            buffer = "[Buffer]"
+                        })[entry.source.name]
+                        return vim_item
+                    end,
                     sources = {
                         {name = "nvim_lsp"},
                         {name = "buffer"}
@@ -164,14 +219,25 @@ return require("packer").startup(
         use {
             "simrat39/rust-tools.nvim",
             requires = {
+                "hrsh7th/cmp-nvim-lsp",
+                "folke/which-key.nvim",
                 "neovim/nvim-lspconfig",
                 "nvim-lua/popup.nvim",
                 "nvim-lua/plenary.nvim",
-                "nvim-lua/telescope.nvim"
-                --"mfussenegger/nvim-dap"
+                "nvim-telescope/telescope.nvim"
             },
             config = function()
-                require("rust-tools").setup {}
+                local cmp_nvim_lsp = require("cmp_nvim_lsp")
+                local capabilities = lsp_get_capabilities(cmp_nvim_lsp)
+                require("rust-tools").setup {
+                    server = {
+                        on_attach = function(client, buffernr)
+                            local wk = require("which-key")
+                            lsp_setup_buffer_keymap(wk)(client, buffernr)
+                        end,
+                        capabilities = capabilities
+                    }
+                }
             end
         }
     end
