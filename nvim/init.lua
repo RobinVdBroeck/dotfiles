d7bb3d6eb36a86cbdf7c405c4628919e1da5e608
Robin Van den Broeck <robin.broeck@gmail.com>
2022-07-12 10:41:49 +0200

    nvim: Add persistent undo

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,24 @@ if vim.fn.has("termguicolors") == 1 then
     vim.cmd("colorscheme dracula")
 end
 
+
 -- todo: we should be able to convert this to lua
+vim.cmd([[
+if has('persistent_undo')
+    " define a path to store persistent undo files.
+    let target_path = expand('~/.config/vim-persisted-undo/')
+    " create the directory and any parent directories
+    " if the location does not exist.
+    if !isdirectory(target_path)
+        call system('mkdir -p ' . target_path)
+    endif
+    " point Vim to the defined undo directory.
+    let &undodir = target_path
+    " finally, enable undo persistence.
+    set undofile
+endif
+]])
+
 vim.cmd([[
 """""""""""""""""""
 " Custom commands "
