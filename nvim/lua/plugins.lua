1e498d4e74e00a69d438646413f28221b3e741fe
Robin Van den Broeck <robin.broeck@gmail.com>
2022-12-20 10:03:12 +0100

    nvim: add vim-fugitive

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -56,6 +56,7 @@ return require("packer").startup(function()
     }
 
     -- Git
+    use "tpope/vim-fugitive"
     use {
         "lewis6991/gitsigns.nvim",
         requires = {"nvim-lua/plenary.nvim"},
