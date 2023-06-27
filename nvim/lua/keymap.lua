182c907254e2028ab4e6b0df5e58a314c04e368c
Robin Van den Broeck <robin.broeck@gmail.com>
2023-06-27 08:50:13 +0200

    nvim: lsp keymapping in lua/keymap.lua

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -38,3 +38,33 @@ map('n', '<C-w>j', ":call WindowMove('j')<CR>")
 map('n', '<C-w>k', ":call WindowMove('k')<CR>")
 map('n', '<C-w>l', ":call WindowMove('l')<CR>")
 -- <C-w>c closes by default
+
+-- Setup keymappings based on buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = function(desc)
+      return {
+        buffer = ev.buf,
+        desc = desc,
+      }
+    end
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts '[g]oto [D]eclaration')
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts '[g]oto [d]efinition')
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts 'hover')
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts '[g]oto [i]mplementation')
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts 'signature help')
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts '[w]orkspace [a]dd folder')
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts '[w]orkspace [r]emove folder')
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts '[w]orkspace [l]ist folder')
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts 'type [D]efintion')
+    vim.keymap.set('n', '<leader>cn', vim.lsp.buf.rename, opts '[c]hange [n]ame')
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts '[c]ode [a]ction')
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts '[g]oto [r]eferences')
+    vim.keymap.set('n', '<leader>cf', function()
+      vim.lsp.buf.format { async = true }
+    end, opts '[c]hange [f]ormat')
+  end,
+})
