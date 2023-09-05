17059dd1f83ffdeaf154a94bf8a45a94b00be0ee
Robin Van den Broeck <robin.broeck@gmail.com>
2023-09-05 09:20:16 +0200

    nvim: highlight what has been yanked

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,3 +64,10 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
     }
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+  group = vim.api.nvim_create_augroup('YankHightlight', {}),
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
