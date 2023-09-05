22b180282134b49c3f1f39a53331679c820619cd
Robin Van den Broeck <robin.broeck@gmail.com>
2023-09-05 09:21:04 +0200

    nvim: don't run treesitter on big files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -224,6 +224,14 @@ return require('lazy').setup {
         ensure_installed = 'all',
         highlight = {
           enable = true,
+          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(_lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
         },
       }
     end,
