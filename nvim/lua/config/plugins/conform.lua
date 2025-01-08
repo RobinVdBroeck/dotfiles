0de49211dfc0d6c56793796bf847288992264b23
Robin Van den Broeck <robin.broeck@gmail.com>
2025-01-08 11:10:21 +0100

    nvim: fix not formatting scss and css in conform + update

--- nvim/lua/config/plugins/conform.lua
+++ nvim/lua/config/plugins/conform.lua
@@ -20,20 +20,26 @@ return {
     },
     opts = {
       async = true,
-      format_on_save = {
-        -- format on save can block for a max of 1 second
-        timeout_ms = 1000,
-        lsp_format = 'fallback',
-      },
+      stop_after_first = true,
+      format_on_save = function(bufnr)
+        local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+        -- Disable auto format on save for scss and css
+        if filetype == 'css' or filetype == 'scss' then
+          return nil
+        end
+        return {
+          -- format on save can block for a max of 1 second
+          timeout_ms = 1000,
+          lsp_format = 'fallback',
+        }
+      end,
       formatters_by_ft = {
         lua = { 'stylelua' },
-        javascript = { { 'eslint_d', 'prettier' }, 'biome' },
-        typescript = { { 'eslint_d', 'prettier' } },
-        typescriptreact = { { 'eslint_d', 'prettier' } },
-        javascriptreact = { { 'eslint_d', 'prettier' } },
+        javascript = { 'eslint_d', 'prettier', 'biome' },
+        typescript = { 'eslint_d', 'prettier' },
+        typescriptreact = { 'eslint_d', 'prettier' },
+        javascriptreact = { 'eslint_d', 'prettier' },
         terraform = { 'tofu_fmt' },
-        -- Don't format scss
-        scss = {},
       },
     },
   },
