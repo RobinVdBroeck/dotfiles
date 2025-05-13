30ef3c129d9c487d69a7f6470b41957b9be3f2ea
Robin Van den Broeck <robin.broeck@gmail.com>
2025-05-13 22:37:14 +0200

    wezterm: update keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,31 +5,59 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'catppuccin-mocha'
 
--- Setup leader key
+-- Leader key: CTRL-a
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.keys = {
-  -- pane management
+  -- Split panes (vim style: s = horizontal split, v = vertical split)
   {
     mods = 'LEADER',
-    key = '%',
+    key = 's',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   {
     mods = 'LEADER',
-    key = '=',
+    key = 'v',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
+
+  -- Pane navigation (vim style: h/j/k/l)
+  {
+    mods = 'LEADER',
+    key = 'h',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    mods = 'LEADER',
+    key = 'j',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    mods = 'LEADER',
+    key = 'k',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    mods = 'LEADER',
+    key = 'l',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+
+  -- Zoom current pane
   {
     mods = 'LEADER',
     key = 'z',
     action = wezterm.action.TogglePaneZoomState,
   },
+
+  -- Rotate panes
   {
     mods = 'LEADER',
     key = 'Space',
     action = wezterm.action.RotatePanes 'Clockwise',
   },
+
+  -- Tab management
   {
     mods = 'LEADER',
     key = 'c',
@@ -52,11 +80,12 @@ config.keys = {
   },
   {
     mods = 'LEADER',
-    key = 'l',
+    key = 't',
     action = wezterm.action.ActivateLastTab,
   },
 }
 
+-- Bind numbers 1-9 to tab activation
 for i = 1, 9 do
   table.insert(config.keys, {
     mods = 'LEADER',
